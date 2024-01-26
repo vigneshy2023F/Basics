b9d05c0d1766b7a3c53ab20fd15d91f424bd2c55
vigneshy2023F <148505714+vigneshy2023F@users.noreply.github.com>
2024-01-26 15:44:53 -0500

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+def fizz_buzz(n):
+    for x in range(1, n+1):
+        if x%3 and i%5 == 0:
+            print ("FizzBuzz")
+        elif x%3 == 0:
+            print ("Fizz")
+        elif x%5 == 0:
+            print ("Buzz")
+        else:
+            print(x)
